9599ae44a1172ba5e0c4a04a7497f06509a2015d
Kasun_Udayanga <udayangakasun696@gmail.com>
2025-01-11 18:36:35 +0530

    start application page

--- client/src/pages/application.jsx
+++ client/src/pages/application.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../Component/Navbar'
 
 const application = () => {
+  const [isEdit,setIsEdit] = useState(false);
   return (
-    <div>application</div>
+    <>
+    <Navbar/>
+    <div>
+      <h2>Your Resume</h2>
+    </div>
+    <div>
+
+    </div>
+    </>
   )
 }
 
